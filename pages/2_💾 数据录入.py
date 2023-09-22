6dad8fab6d05675fa8ec091500c87783a16a9d50
gll2titan <48703290+gll2titan@users.noreply.github.com>
2023-09-22 13:18:39 +0800

    Update 2_💾 数据录入.py

--- pages/2_💾 数据录入.py
+++ pages/2_💾 数据录入.py
@@ -4,7 +4,7 @@ def in_data():
     import openpyxl
 
     st.markdown("# 💾 数据录入")
-    st.markdown(:rainbow[目前不可用,请不要使用!] :balloon:)
+    st.markdown(":rainbow[目前不可用,请不要使用!] :balloon:")
 
     def in_data_m():
         # 读取上传的Excel文件
